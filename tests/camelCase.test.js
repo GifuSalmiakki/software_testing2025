e38b4cf49eda4e8dfc8e3ff91efada59e973d1fd
sisu suominen <sisu.suominen@tuni.fi>
2025-12-09 00:01:47 +0200

    Organized files

---
+++ tests/camelCase.test.js
@@ -0,0 +1,23 @@
+import camelCase from "./camelCase.js";
+
+describe("camelCase()", () => {
+  it("converts normal strings", () => {
+    expect(camelCase("Foo Bar")).toBe("fooBar");
+  });
+
+  it("converts dashed input", () => {
+    expect(camelCase("--foo-bar--")).toBe("fooBar");
+  });
+
+  it("converts snake case", () => {
+    expect(camelCase("__FOO_BAR__")).toBe("fooBar");
+  });
+
+  it("returns empty string on empty input", () => {
+    expect(camelCase("")).toBe("");
+  });
+
+  it("handles non-string input per your implementation", () => {
+    expect(camelCase(123)).toBe("123");
+  });
+});
