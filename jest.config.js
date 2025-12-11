9aee476554c8436c3ea32cbbf43a1d4f685a0e5e
sisu suominen <sisu.suominen@tuni.fi>
2025-12-11 13:29:31 +0200

    Configured jest to only calculate coverage for the files chosen for testing

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ export default {
   testEnvironment: "node",           // Node environment
   transform: {},                      // no transform needed
   collectCoverage: true,              // enable coverage
-  collectCoverageFrom: ["src/**/*.js"],
+  collectCoverageFrom: ["src/add.js", "src/camelCase.js", "src/ceil.js", "src/compact.js", "src/isEmpty.js", "src/toFinite.js", "src/toNumber.js", "src/reduce.js", "src/filter.js"],
   coverageDirectory: "coverage",
   coverageReporters: ["json", "lcov", "text", "clover"],
   testMatch: ["**/tests/**/*.test.js"],  // test files pattern
